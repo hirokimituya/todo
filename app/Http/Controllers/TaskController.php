e2afbfaa4ecdfe7ac2435a65cbadac2f9a2dc558
hirokimituya <hiromitu819@yahoo.co.jp>
2021-03-25 23:24:14 +0900

    タスク編集機能 追加
    ・タスク編集機能のルート、コントローラアクション、テンプレート、バリデーション、テスト追加。
    ・テンプレートの部品化。

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\CreateTask;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests\EditTask;
+
 class TaskController extends Controller
 {
     public function index(int $id) 
@@ -50,6 +52,32 @@ class TaskController extends Controller
 
         return redirect()->route('tasks.index', [
             'id' => $current_folder->id,
+        ]);
+    }
+
+    /**
+     * GET /folders/{id}/tasks/{task_id}/edit
+     */
+    public function showEditForm(int $id, int  $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
+
+    public function edit(int $id, int $task_id,  EditTask $request)
+    {
+        $task = Task::find($task_id);
+
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+        
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
         ]);
     }
 }
